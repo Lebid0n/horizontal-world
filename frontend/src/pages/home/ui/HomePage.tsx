820e44d82e45d4ac09c3145c3786702c259ed26a
rorinuma <focusfire2650@gmail.com>
2025-09-09 11:57:17 +0300

    global variables, inline theme and dark/light theme support

--- frontend/src/pages/home/ui/HomePage.tsx
+++ frontend/src/pages/home/ui/HomePage.tsx
@@ -1,3 +1,9 @@
 export default function HomePage() {
-  return <></>
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="p-3 flex flex-col w-[500px] h-[400px] bg-black-secondary border-t-8 border-orange-primary">
+        <h1 className="text-4xl font-bold text-orange-primary">Horizontal World</h1>
+      </div>
+    </div>
+  )
 }
