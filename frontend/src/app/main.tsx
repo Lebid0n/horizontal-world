c63ac8438333594aa9ac3d87e56deea97cd30dfe
rorinuma <focusfire2650@gmail.com>
2025-09-09 22:29:25 +0300

    outlet

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -1,24 +1,23 @@
-// react
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//css
 import './index.css'
-// router
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// pages
 import HomePage from '../pages/home/ui/HomePage'
 import ProjectInfoPage from '../pages/project-info/ui/ProjectInfoPage'
 import AuthPage from '../pages/auth/ui/AuthPage'
 import UserProfilePage from '../pages/user-profile/ui/UserProfilePage'
+import Layout from '@/shared/layout/Layout'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/info" element={<ProjectInfoPage />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/profile" element={<UserProfilePage />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/" index element={<HomePage />} />
+          <Route path="/info" element={<ProjectInfoPage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/profile" element={<UserProfilePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
