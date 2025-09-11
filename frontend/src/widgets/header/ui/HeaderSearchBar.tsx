9d265f7d2251881298465a72aab5ebbf0d67c6dc
Amir <lebido_on@mail.ru>
2025-09-11 15:11:10 +0000

    Colors bug fixed

---
+++ frontend/src/widgets/header/ui/HeaderSearchBar.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import type { ChangeEvent, KeyboardEvent, MouseEvent } from "react";
+import { CiSearch } from "react-icons/ci";
+import { RxCross1 } from "react-icons/rx";
+
+export default function HeaderSearchBar() {
+    const [searchBarText, setSearchBarText] = useState("");
+    
+    const handleSearch = (e: MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        if (searchBarText.trim()) {
+            console.log("Searching for:", searchBarText);
+        }
+    }
+
+    const clearSearch = (e: MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        setSearchBarText("");
+    }
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchBarText(e.target.value);
+    }
+
+    const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            if (searchBarText.trim()) {
+                console.log("Searching for:", searchBarText);
+            }
+        }
+    }
+
+    return (
+        <div className="w-[40vw] h-[15vh] flex justify-center items-center bg-bg-tertiary">
+            <button 
+                onClick={handleSearch}
+                disabled={!searchBarText.trim()}
+                className="w-[10%] h-[45%] flex justify-center items-center rounded-l-4xl bg-accent-200 transition-[0.5s] hover:bg-accent-300 disabled:cursor-not-allowed"
+            >
+                <CiSearch className="w-[1.5vw] h-[1.5vw]" />
+            </button>
+            
+            <input 
+                type="text" 
+                value={searchBarText}
+                onChange={handleInputChange}
+                onKeyPress={handleKeyPress}
+                placeholder="Новые горизонты?"
+                className="w-[70%] h-[45%] p-4 bg-accent-200 text-text-primary placeholder-text-secondary transition-[0.5s] hover:bg-accent-300 focus:bg-accent-400 outline-0"
+            />
+            
+            <div className="w-[10%] h-[45%] flex justify-center items-center bg-accent-200 rounded-r-4xl transition-[0.5s] hover:bg-accent-300">
+                {searchBarText == "" ? 
+                    <button 
+                        onClick={clearSearch}
+                        disabled={!searchBarText}
+                        className="flex justify-center items-center disabled:cursor-not-allowed"
+                    >
+                    </button>
+                    :
+                    <button 
+                        onClick={clearSearch}
+                        disabled={!searchBarText}
+                        className="flex justify-center items-center disabled:cursor-not-allowed"
+                    >
+                        <RxCross1 className="w-[1.5vw] h-[1.5vw]" />
+                    </button>
+                }
+            </div>
+        </div>
+    )
+}
