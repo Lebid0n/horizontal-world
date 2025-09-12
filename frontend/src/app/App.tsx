9154ff9584d84d5725c3b9bb757407331934c69c
rorinuma <focusfire2650@gmail.com>
2025-09-12 17:18:55 +0300

    feat: added a separate fallback page for an exercise,that activates when the user navigates the link directly

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -4,11 +4,14 @@ import Layout from '@/shared/layout/Layout'
 import ExercisesPage from '@/pages/exercises/ui/ExercisesPage'
 import UserProfilePage from '@/pages/user-profile/ui/UserProfilePage'
 import ExerciseModal from '@/pages/exercises/ui/ExerciseModal'
+import ExercisePage from '@/pages/exercises/ui/ExercisePage'
 
 export default function App() {
   const location = useLocation()
   const previousLocation = location.state?.location
   const exercise = location.state?.exercise
+
+  console.log(previousLocation)
 
   return (
     <>
@@ -17,6 +20,7 @@ export default function App() {
           <Route index element={<ProjectInfoPage />} />
           <Route path="/exercises" element={<ExercisesPage />} />
           <Route path="/profile/:id" element={<UserProfilePage />} />
+          {!previousLocation && <Route path="/exercises/:slug" element={<ExercisePage />} />}
         </Route>
       </Routes>
       {previousLocation && (
