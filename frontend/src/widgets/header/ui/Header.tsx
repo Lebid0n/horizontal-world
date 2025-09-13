c20654a184a15b85580ad6b3bcfb7bb5b627b94c
Amir <lebido_on@mail.ru>
2025-09-13 11:05:27 +0000

    adaptive design 1.0

--- frontend/src/widgets/header/ui/Header.tsx
+++ frontend/src/widgets/header/ui/Header.tsx
@@ -1,17 +1,33 @@
+import { IoMenu } from "react-icons/io5";
 import HeaderSearchBar from './HeaderSearchBar'
+import { useMediaQuery } from 'react-responsive';
 
 export default function Header() {
+  const isDesktop = useMediaQuery({minWidth: 1024})
+
   return (
-    <header className="w-[100vw] h-[15vh] flex bg-bg-secondary">
-      <div className="w-[20vw] h-full flex justify-center items-center p-4 bg-bg-tertiary">
-        <h1 className="text-3xl font-bold select-none">Horizonts World</h1>
+    <header className="w-full h-[15vh] flex bg-bg-tertiary items-center p-5">
+      {isDesktop && 
+        <div className="w-[30vw]">
+          <h1 className="text-3xl font-bold select-none">Horizontal World</h1>
+        </div>
+      }
+      <div className="w-full flex justify-center">
+        <HeaderSearchBar />
       </div>
-      <HeaderSearchBar />
-      <div className="w-[40vw] h-full flex justify-end items-center p-4 gap-4 bg-bg-tertiary">
-        <div className="w-[5vw] h-[5vw] bg-accent-300 rounded-full"></div>
-        <div className="w-[5vw] h-[5vw] bg-accent-300 rounded-full"></div>
-        <div className="w-[5vw] h-[5vw] bg-accent-300 rounded-full"></div>
-      </div>
+      {isDesktop ? (
+        <div className="w-[30%] flex justify-center items-center gap-4 bg-bg-tertiary">
+          <div className="w-12 h-12 bg-accent-300 rounded-full"></div>
+          <div className="w-12 h-12 bg-accent-300 rounded-full"></div>
+          <div className="w-12 h-12 bg-accent-300 rounded-full"></div>
+        </div>
+      ) : (
+        <div className="w-[30%] h-full flex justify-center items-center bg-bg-tertiary">
+          <div className='h-[60%] aspect-square bg-accent-300 rounded-full flex items-center justify-center'>
+            <IoMenu size={30} />
+          </div>
+        </div>
+      )}
     </header>
   )
 }
--- frontend/src/widgets/header/ui/HeaderSearchBar.tsx
+++ frontend/src/widgets/header/ui/HeaderSearchBar.tsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import type { ChangeEvent, KeyboardEvent, MouseEvent } from 'react'
 import { CiSearch } from 'react-icons/ci'
 import { RxCross1 } from 'react-icons/rx'
+import { useMediaQuery } from 'react-responsive'
 
 export default function HeaderSearchBar() {
+  const isDesktop = useMediaQuery({minWidth: 1024})
   const [searchBarText, setSearchBarText] = useState('')
 
   const handleSearch = (e: MouseEvent<HTMLButtonElement>) => {
@@ -32,13 +34,13 @@ export default function HeaderSearchBar() {
   }
 
   return (
-    <div className="w-[40vw] h-[15vh] flex justify-center items-center bg-bg-tertiary">
+    <div className="w-full h-[15vh] flex justify-center items-center bg-bg-tertiary">
       <button
         onClick={handleSearch}
         disabled={!searchBarText.trim()}
         className="w-[10%] h-[45%] flex justify-center items-center rounded-l-4xl bg-accent-200 transition-[0.5s] hover:bg-accent-300 disabled:cursor-not-allowed"
       >
-        <CiSearch className="w-[1.5vw] h-[1.5vw]" />
+        <CiSearch className="w-[50%] h-[50%]" />
       </button>
 
       <input
@@ -46,27 +48,24 @@ export default function HeaderSearchBar() {
         value={searchBarText}
         onChange={handleInputChange}
         onKeyDown={handleKeyPress}
-        placeholder="Новые горизонты?"
-        className="w-[70%] h-[45%] p-4 bg-accent-200 text-text-primary placeholder-text-secondary transition-[0.5s] hover:bg-accent-300 focus:bg-accent-400 outline-0"
+        placeholder={isDesktop ? "Новые горизонты?" : "?"}
+        className="w-[80%] h-[45%] max-w-[70%] pl-4 bg-accent-200 text-text-primary placeholder-text-secondary transition-[0.5s] hover:bg-accent-300 focus:bg-accent-400 outline-0"
       />
-
-      <div className="w-[10%] h-[45%] flex justify-center items-center bg-accent-200 rounded-r-4xl transition-[0.5s] hover:bg-accent-300">
-        {searchBarText == '' ? (
-          <button
-            onClick={clearSearch}
-            disabled={!searchBarText}
-            className="flex justify-center items-center disabled:cursor-not-allowed"
-          ></button>
-        ) : (
-          <button
-            onClick={clearSearch}
-            disabled={!searchBarText}
-            className="flex justify-center items-center disabled:cursor-not-allowed"
-          >
-            <RxCross1 className="w-[1.5vw] h-[1.5vw]" />
-          </button>
-        )}
-      </div>
+      {searchBarText == '' ? (
+        <button
+          onClick={clearSearch}
+          disabled={!searchBarText}
+          className="w-[10%] h-[45%] flex justify-center items-center rounded-r-4xl bg-accent-200 transition-[0.5s] hover:bg-accent-300 disabled:cursor-not-allowed"
+        ></button>
+      ) : (
+        <button
+          onClick={clearSearch}
+          disabled={!searchBarText}
+          className="w-[10%] h-[45%] flex justify-center items-center rounded-r-4xl bg-accent-200 transition-[0.5s] hover:bg-accent-300 disabled:cursor-not-allowed"
+        >
+          <RxCross1 className="w-[50%] h-[50%]" />
+        </button>
+      )}
     </div>
   )
 }
